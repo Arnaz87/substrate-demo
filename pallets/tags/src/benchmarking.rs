840b827ef9be33cf94143475891000b0e7844a33
Shivering Ocelot <shiveringocelot@pm.me>
2024-05-27 13:30:59 -0500

    Benchmark initial code. WIP

--- pallets/tags/src/benchmarking.rs
+++ pallets/tags/src/benchmarking.rs
@@ -6,29 +6,51 @@ use super::*;
 use crate::Pallet as Template;
 use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
+use frame_support::traits::Get;
+use sp_core::bounded::BoundedVec;
+use sp_std::vec::vec;
+
+macro_rules! bvec {
+	($( $x:tt )*) => {
+		TryInto::<BoundedVec<_, _>>::try_into(vec![$( $x )*]).unwrap()
+	}
+}
 
 #[benchmarks]
 mod benchmarks {
 	use super::*;
 
 	#[benchmark]
-	fn do_something() {
-		let value = 100u32.into();
+	fn create_tag() {
 		let caller: T::AccountId = whitelisted_caller();
-		#[extrinsic_call]
-		do_something(RawOrigin::Signed(caller), value);
+		let tag_index = TagIndex::<T>::get();
+		let deposit = T::TagDepositAmount::get();
+		let name = bvec![];
 
-		assert_eq!(Something::<T>::get(), Some(value));
+		#[extrinsic_call]
+		create_tag(RawOrigin::Signed(caller), name.clone());
+
+		assert_eq!(TagMap::<T>::try_get(tag_index), Ok(
+			(name.clone(), 1, deposit)
+		));
 	}
 
 	#[benchmark]
-	fn cause_error() {
-		Something::<T>::put(100u32);
+	fn destroy_tag() {
 		let caller: T::AccountId = whitelisted_caller();
-		#[extrinsic_call]
-		cause_error(RawOrigin::Signed(caller));
+		let tag_index = TagIndex::<T>::get();
+		let deposit = T::TagDepositAmount::get();
+		let name = bvec![];
 
-		assert_eq!(Something::<T>::get(), Some(101u32));
+		create_tag(RawOrigin::Signed(caller), name.clone());
+		assert_eq!(TagMap::<T>::get(tag_index), Some(
+			(name.clone(), 1, deposit)
+		));
+
+		#[extrinsic_call]
+		destroy_tag(RawOrigin::Signed(caller), tag_index);
+
+		assert_eq!(TagMap::<T>::get(tag_index), None);
 	}
 
 	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
